901757dfbc4a5cc9aaa2bd61f102cef9ac0ea5d3
charinya <ch.charinya_st@tni.ac.th>
2015-12-01 16:56:48 +0700

    Boom

---
+++ ClassUser.py
@@ -0,0 +1,12 @@
+class User(object):
+    def __init__(self,u_id,u_firstName,u_lastname,u_username,u_password,u_sex,u_mail,u_age,u_joindate,u_post):
+        self.u_id = u_id
+        self.u_firstName = u_firstName
+        self.u_lastname = u_lastname
+        self.u_username = u_username
+        self.u_password = u_password
+        self.u_sex = u_sex
+        self.u_mail = u_mail
+        self.u_age = u_age
+        self.u_joindate = u_joindate
+        self.u_post = u_post
