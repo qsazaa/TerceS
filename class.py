c4a32f6bd8130cfc5ceb0109e97d5684e3c045f4
Sophanat Phokaenkaew <YukiYuuki@users.noreply.github.com>
2015-12-01 16:39:40 +0700

    Update class.py

--- class.py
+++ class.py
@@ -7,5 +7,10 @@ class user(object):
 		
 class TypeMarket(object):
   def __init__(self,typename,license):
-    self.typename = typename
-    self.license = license
+    	self.typename = typename
+    	self.license = license
+class Review(object):
+	def __init__(self,reviewid,suggestion,comment):
+ 		self.reviewid = reviewid
+ 		self.suggestion = suggestion
+ 		self.comment = comment
