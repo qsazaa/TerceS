7e9f23bcb4e912eea1a17400c3840b6f8b7558e6
Sophanat Phokaenkaew <YukiYuuki@users.noreply.github.com>
2015-12-01 16:38:23 +0700

    Create class.py

---
+++ class.py
@@ -0,0 +1,11 @@
+class user(object):
+	def __init__(self,id,password):
+		self.id= id
+		self.password=password
+		self.data = data
+		self.info = info
+		
+class TypeMarket(object):
+  def __init__(self,typename,license):
+    self.typename = typename
+    self.license = license
